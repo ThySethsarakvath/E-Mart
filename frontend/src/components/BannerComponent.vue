f05224dbb57f9db4686474a864ae36254bcf7f56
vath <bro.vath7547@gmail.com>
2025-12-27 15:07:35 +0700

    First Api from Backend to Frontend

---
+++ frontend/src/components/BannerComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="banner">
+    <h2>{{ title }}</h2>
+    <p>{{ subtitle }}</p>
+    <img :src="image"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BannerComponent',
+    props: {
+      title: String,
+      subtitle: String,
+      image: String,
+    },
+  }
+</script>
+
+<style scoped>
+.banner{
+  border: 2px solid black;
+  border-color:black;
+  height: 650px;
+  width: 1750px;
+  margin-top: 40px;
+}
+</style>
