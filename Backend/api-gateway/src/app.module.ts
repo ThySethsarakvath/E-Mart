ad94129d71af016a5a4441b2893bdb54be7f0420
vath <bro.vath7547@gmail.com>
2026-02-04 11:02:11 +0700

    Backend: Fixing Api resolved problems

--- Backend/api-gateway/src/app.module.ts
+++ Backend/api-gateway/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { AuthModule } from './auth/auth.module';
+import { GatewayController } from './gateway/gateway.controller';
+import { ProxyService } from './proxy/proxy.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+    }),
+    HttpModule,
+    AuthModule,
+  ],
+  controllers: [GatewayController, AppController],
+  providers: [ProxyService, AppService],
 })
 export class AppModule {}
