52cdd9b9946fb796fbaa9500a207a70c32cce395
vath <bro.vath7547@gmail.com>
2026-02-15 15:23:03 +0700

    Backend: Modify JWT strategy-Order Worker

--- Backend/order-worker/src/auth/strategy/jwt.strategy.ts
+++ Backend/order-worker/src/auth/strategy/jwt.strategy.ts
@@ -23,7 +23,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
 
     return {
-      userId: payload.sub,
+      id: payload.sub,
       email: payload.email,
       roles: payload.roles || [],
       permissions: payload.permissions || [],
